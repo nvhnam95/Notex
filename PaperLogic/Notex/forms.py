f990a28da5f659697358c7b81e63ec512b17f25c
Nam Nguyen <nvhainam95@gmail.com>
2018-07-28 11:08:24 +0700

    Login, Register function, without styles

--- PaperLogic/Notex/forms.py
+++ PaperLogic/Notex/forms.py
@@ -12,6 +12,11 @@ class LoginForm(forms.Form):
         max_length = 32,
         widget = forms.PasswordInput()
     )
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if " " in username:
+            raise forms.ValidationError("Your username or password are incorrect, please try again.")
+        return username
 
 class RegisterForm(forms.Form):
     username = forms.CharField(
@@ -33,3 +38,19 @@ class RegisterForm(forms.Form):
         max_length = 32,
         widget = forms.PasswordInput()
     )
+
+
+    def clean(self):
+        username = self.cleaned_data['username']
+        password =  self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+
+        if password != confirm_password:
+            raise forms.ValidationError("Password does not match the confirm password.")
+
+        if (User.objects.filter(username=username).exists()):
+            raise forms.ValidationError("Someone took this username, please try another.")
+
+        if " " in password or " " in username:
+            raise forms.ValidationError("Username and password cannot contain space.")
+  
--- PaperLogic/Notex/views.py
+++ PaperLogic/Notex/views.py
@@ -8,43 +8,48 @@ from django import forms
 from Notex.forms import LoginForm, RegisterForm
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == "POST":
+        login_form = LoginForm(method.POST)
+        register_form = RegisterForm(method.POST)
+        return HttpResponseRedirect('/')
+    else:
+        login_form = LoginForm()
+        register_form = RegisterForm()
+        return render(request, 'home.html', {'login_form' : login_form, 'register_form' : register_form})
 
 def log_in(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            userObj = form.cleaned_data
-            username = userObj['username']
-            password =  userObj['password']
-            user = authenticate(username = username, password = password)
+            username = form.cleaned_data['username']
+            password =  form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
             if user:
                 login(request, user)
                 return HttpResponseRedirect('/')
-            else:
-                raise forms.ValidationError('The username or password is incorrect, please try again.')
+
+        login_form = LoginForm(request.POST)
+        register_form = RegisterForm()
+        return render(request, 'home.html', {'login_form' : login_form, 'register_form' : register_form})
     else:
-        form = LoginForm()
-    return render(request, 'login.html', {'form' : form})
+        return HttpResponseRedirect('/')
 
 def log_out(request):
     logout(request)
     return HttpResponseRedirect('/')
 
-
 def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            userObj = form.cleaned_data
-            username = userObj['username']
-            password =  userObj['password']
-            confirm_password = userObj['confirm_password']
-            if not (User.objects.filter(username=username).exists()):
-                User.objects.create_user(username=username, password=password)
-                return HttpResponseRedirect('/')
-            else:
-                raise forms.ValidationError('Looks like a username with that email or password already exists')
+            username = form.cleaned_data['username']
+            password =  form.cleaned_data['password']
+            User.objects.create_user(username=username, password=password)
+            return HttpResponseRedirect('/')
+        else:
+            login_form = LoginForm()
+            register_form = RegisterForm(request.POST)
+            return render(request, 'home.html', {'login_form' : login_form, 'register_form' : register_form})
+
     else:
-        form = RegisterForm()
-    return render(request, 'register.html', {'form' : form})
+        return HttpResponseRedirect('/')
